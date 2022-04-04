f353d8c5ae5947bb22e86e6faff0f874d4ae9716
Rene <rene.guca@hotmail.com>
2022-04-04 19:54:51 +0200

    trip list design impl

---
+++ src/page/TripList.php
@@ -0,0 +1,44 @@
+<?php
+namespace page;
+
+class TripList {
+
+    static function render(array $items) {
+        ?>
+        <div class="trip-list">
+            <?php
+            foreach ($items as $item) self::renderItem($item);
+            ?>
+        </div>
+        <?php
+    }
+
+    static function renderItem(object $item) {
+        $date = isset($item->departDate) ? (new \DateTime($item->departDate))->format('d.m.') : 'DATE';
+        ?>
+        <a class="item" href="<?= $item->deeplink ?? '' ?>">
+            <div>
+                <p><?= $date ?></p>
+                <p><?= $item->departTime ?? 'TIME' ?></p>
+            </div>
+            <ul>
+                <?php
+                foreach ($item->stops ?? [] as $stop) {
+                    ?>
+                    <li><?= $stop->address ?? '' ?></li>
+                    <?php
+                }
+                ?>
+            </ul>
+            <img class="logo" src="<?= self::getLogoUrl($item) ?>">
+        </a>
+        <?php
+    }
+
+    static function getLogoUrl(object $item) {
+        if (str_contains($item->deeplink ?? '', 'mifaz'))
+            return "https://ride2go.com/img/mifaz_logo.png";
+        return "https://ride2go.com/img/r2g_favicon.png";
+    }
+
+}
---
+++ src/public/iframe2.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>iframe</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.min.css">
+    <meta name="iframe" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div id="iframe-container">
+    <?php
+    use page\TripList;
+
+    require_once "../core/Autoload.php";
+
+    $filedata = file_get_contents('./data/json_data.json');
+    $details = json_decode($filedata); ?>
+    <?php
+    TripList::render($details);
+    ?>
+</div>
+</body>
+</html>
