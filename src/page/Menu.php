4a5b35686cfc4cbc3c4a371ae4174820c84979e8
Rene <rene.guca@hotmail.com>
2022-04-25 23:39:24 +0200

    removed faq page; narrower lang menu

--- src/page/Menu.php
+++ src/page/Menu.php
@@ -11,7 +11,6 @@ class Menu {
             <a href="/legal-notice.php"><p><?= Label::get('legalNotice.title') ?></p></a>
             <a href="/privacy-policy.php"><p><?= Label::get('privacyPolicy.title') ?></p></a>
             <a href="mailto:contact@ridewithukraine.eu"><p><?= Label::get('contact.title') ?></p></a>
-            <a href="/faq.php"><p><?= Label::get('faq.title') ?></p></a>
             <a href="/press.php"><p><?= Label::get('press.title') ?></p></a>
         </div>
         <?php
