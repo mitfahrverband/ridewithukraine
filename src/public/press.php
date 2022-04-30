e38e0780a7ef72c94a0e6d008439da690accc7da
Rene <rene.guca@hotmail.com>
2022-04-30 20:20:28 +0200

    added press images

--- src/public/press.php
+++ src/public/press.php
@@ -9,8 +9,21 @@ Page::addStylesheet('/css/text.css');
 
 Page::render(function () {
     ?>
-    <div class="col-span-3 mx-3">
+    <div id="press" class="col-span-3 mx-3">
         <h1>Presse</h1>
+        <div class="images">
+            <?php
+            $files = glob(__DIR__ . '/img/press/*.*');
+            foreach ($files as $file) {
+                $file = basename($file);
+                ?>
+                <a href="/img/press/<?= $file ?>">
+                    <img src="/img/press/scaled/<?= $file ?>.w480.jpg">
+                </a>
+                <?php
+            }
+            ?>
+        </div>
     </div>
     <?php
 });
