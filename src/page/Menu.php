305b6fb371a324b832be564c5965fc9ab21e2df8
Martin <1383111+MartinH-open@users.noreply.github.com>
2022-05-03 14:32:27 +0200

    Update Menu.php
    open new blank page for social media and add subject to email to allow reference to page.

--- src/page/Menu.php
+++ src/page/Menu.php
@@ -13,11 +13,11 @@ class Menu {
             <a href="mailto:contact@ridewithukraine.eu"><p><?= Label::get('contact.title') ?></p></a>
             <a href="/press.php"><p><?= Label::get('press.title') ?></p></a>
             <div class="social">
-                <a href="mailto:contact@ridewithukraine.eu"
+                <a href="mailto:contact@ridewithukraine.eu?subject=RideWithUkraine: "
                 ><img src="/img/social/socialmedia-contact-icons00005.svg"></a>
-                <a href="https://www.instagram.com/ridewithukraine/"
+                <a href="https://www.instagram.com/ridewithukraine/" target="_blank"
                 ><img src="/img/social/socialmedia-contact-icons00001.svg"></a>
-                <a href="https://www.twitter.com/ridewithukraine"
+                <a href="https://www.twitter.com/ridewithukraine" target="_blank"
                 ><img src="/img/social/socialmedia-contact-icons00002.svg"></a>
             </div>
         </div>
